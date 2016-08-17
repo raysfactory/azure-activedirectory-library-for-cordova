35f1db16907ac23427d75095346e7d9a9c7365fb
Joshua Noel <jnoel@leviton.com>
2016-08-17 16:25:29 -0500

    added new 3.0.0-pre.2 generated ADALiOS lib file, added instructions on how to get it

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,18 @@ var shell = require("gulp-shell");
 var git = require("gulp-git");
 var runSequence = require("gulp-run-sequence");
 
+
+// To get framework file for ios:
+// 1) Build
+// 2) Right-click "libADALiOS.a" in xcode
+// 3) click "Show in Finder"
+// 4) copy source file into src/ios/
+
+// NOTE: build product will be at:
+// /Libary/Developer/Xcode/DerivedData/ADALiOS-*******/Build/Products/Debug-iphoneos/libADALiOS.a
+
+// TODO: automate process for moving product output to src/ios folder
+
 var ADALiOS = {
     name: "azure-activedirectory-library-for-objc",
     path: "./src/ios/ADALiOS",
@@ -11,15 +23,15 @@ var ADALiOS = {
 };
 
 gulp.task("ios-clean-adal", function(){
-    return del(ADALiOS.path);
+    return del([ADALiOS.name, ADALiOS.path]);
 });
 
 gulp.task("ios-pull-adal", function(done){
-    git.clone("https://github.com/AzureAD/" + ADALiOS.name + ".git", {args: ADALiOS.path}, done);
+    git.clone("https://github.com/AzureAD/" + ADALiOS.name + ".git", null, done);
 });
 
 gulp.task("ios-checkout-adal", function(done){
-    git.checkout("tags/" + ADALiOS.tag, {cwd: ADALiOS.path}, done);
+    git.checkout("tags/" + ADALiOS.tag, {cwd: ADALiOS.name}, done);
 });
 
 gulp.task("ios-get-adal", function(done){
