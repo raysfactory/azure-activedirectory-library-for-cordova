9be4fe4da524098e4b2aedb73846fba67235a9ff
Joshua Noel <jnoel@leviton.com>
2016-08-18 17:01:19 -0500

    updated ADALiOS references, fully automated update process

--- gulpfile.js
+++ gulpfile.js
@@ -8,36 +8,41 @@ var runSequence = require("gulp-run-sequence");
  * ADALiOS update flow:
  *
  * 1) npm install (only if not already installed)
- * 2) gulp ios-get-adal
- * 3) cd azure-activedirectory-library-for-objc
- * 4) ./build_sdk.sh
- * 5) cd ..
- * 6) gulp ios-transfer-adal
+ * 2) gulp ios-adal
  */
 
 var ADALiOS = {
-    name: "azure-activedirectory-library-for-objc",
-    path: "./src/ios/ADALiOS",
+    dir: "azure-activedirectory-library-for-objc",
     tag: "3.0.0-pre6",
     script: "build_sdk.sh",
     framework: "ADALiOS.framework"
 };
 
 gulp.task("ios-clean-adal", function(){
-    return del([ADALiOS.name, ADALiOS.path]);
+    return del(ADALiOS.dir);
 });
 
 gulp.task("ios-pull-adal", function(done){
-    git.clone("https://github.com/AzureAD/" + ADALiOS.name + ".git", null, done);
+    git.clone("https://github.com/AzureAD/" + ADALiOS.dir + ".git", null, done);
 });
 
 gulp.task("ios-checkout-adal", function(done){
-    git.checkout("tags/" + ADALiOS.tag, {cwd: ADALiOS.name}, done);
+    git.checkout("tags/" + ADALiOS.tag, {cwd: ADALiOS.dir}, done);
 });
 
 gulp.task("ios-move-script", function(){
     return gulp.src("./src/ios/tools/" + ADALiOS.script)
-    .pipe(gulp.dest("./" + ADALiOS.name));
+    .pipe(gulp.dest("./" + ADALiOS.dir));
+});
+
+gulp.task("ios-run-script", shell.task("./" + ADALiOS.script, {cwd: ADALiOS.dir}));
+
+gulp.task("ios-script", function(done){
+    runSequence(
+        "ios-move-script",
+        "ios-run-script",
+        done
+    );
 });
 
 gulp.task("ios-get-adal", function(done){
@@ -45,12 +50,20 @@ gulp.task("ios-get-adal", function(done){
         "ios-clean-adal",
         "ios-pull-adal",
         "ios-checkout-adal",
-        "ios-move-script",
+        "ios-script",
         done
     );
 });
 
 gulp.task("ios-transfer-adal", function(){
-    return gulp.src("./" + ADALiOS.name + "/build/" + ADALiOS.framework)
-    .pipe(gulp.dest("./src/ios"));
+    return gulp.src("./" + ADALiOS.dir + "/build/" + ADALiOS.framework + "/*", {read: false})
+    .pipe(gulp.dest("./src/ios/" + ADALiOS.framework));
+});
+
+gulp.task("ios-update-adal", function(done){
+    runSequence(
+        "ios-get-adal",
+        "ios-transfer-adal",
+        done
+    );
 });
