f923dbae7e98a9c31fdb1e24c02bad1ef17ccc23
Joshua Noel <jnoel@leviton.com>
2016-08-18 17:05:30 -0500

    fine-tuned ADALiOS update process

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ var runSequence = require("gulp-run-sequence");
  * ADALiOS update flow:
  *
  * 1) npm install (only if not already installed)
- * 2) gulp ios-adal
+ * 2) gulp ios-update-adal
  */
 
 var ADALiOS = {
@@ -27,7 +27,21 @@ gulp.task("ios-pull-adal", function(done){
 });
 
 gulp.task("ios-checkout-adal", function(done){
-    git.checkout("tags/" + ADALiOS.tag, {cwd: ADALiOS.dir}, done);
+    if(ADALiOS.tag){
+        git.checkout("tags/" + ADALiOS.tag, {cwd: ADALiOS.dir}, done);
+    } else {
+        console.log("no tag found, skipping checkout");
+        done();
+    }
+});
+
+gulp.task("ios-get-adal", function(done){
+    runSequence(
+        "ios-clean-adal",
+        "ios-pull-adal",
+        "ios-checkout-adal",
+        done
+    );
 });
 
 gulp.task("ios-move-script", function(){
@@ -45,16 +59,6 @@ gulp.task("ios-script", function(done){
     );
 });
 
-gulp.task("ios-get-adal", function(done){
-    runSequence(
-        "ios-clean-adal",
-        "ios-pull-adal",
-        "ios-checkout-adal",
-        "ios-script",
-        done
-    );
-});
-
 gulp.task("ios-transfer-adal", function(){
     return gulp.src("./" + ADALiOS.dir + "/build/" + ADALiOS.framework + "/*", {read: false})
     .pipe(gulp.dest("./src/ios/" + ADALiOS.framework));
@@ -63,6 +67,7 @@ gulp.task("ios-transfer-adal", function(){
 gulp.task("ios-update-adal", function(done){
     runSequence(
         "ios-get-adal",
+        "ios-script",
         "ios-transfer-adal",
         done
     );
